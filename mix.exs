179082ecddf52ef85fa9dbec2b299dfe017b071f
Karl Eklund <localpart@gmail.com>
2018-06-13 12:34:03 +0200

    bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Gigatester.Mixfile do
   def project do
     [
       app: :gigatester,
-      version: "0.0.1",
+      version: "0.0.2",
       elixir: "~> 1.4",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
